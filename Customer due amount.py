83a64d784af9daf5873ec38fe0ea5e648c665ea7
Saadisalive <snafiullah@gmail.com>
2025-04-05 19:06:20 +0500

    Add files via upload

---
+++ Customer due amount.py
@@ -0,0 +1,15 @@
+def func(a,b,c,d,e,f,g,h,i,j):
+    return(a+b+c+d+e+f+g+h+i+j)
+
+a = int(input("put your first item value :"))
+b = int(input("put your sec item value :"))
+c = int(input("put your third item value :"))
+d = int(input("put your fourth item value :"))
+e = int(input("put your fifth item value :"))
+f =int(input("put your sixth item value :"))
+g = int(input("put your seventh item value :"))
+h = int(input("put your eight item value :"))
+i =int(input("put your ninth item value :"))
+j = int(input("put your tenth item value :"))
+sum =func(a,b,c,d,e,f,g,h,i,j)
+print(sum)
